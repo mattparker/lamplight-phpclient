d72563bd9fd61330d04af5858effcb205eb85794
Matt Parker <matt@lamplightdb.co.uk>
2012-07-31 19:15:48 +0100

    Docblock and README updates for version 1.2

--- Record/WorkSummary.php
+++ Record/WorkSummary.php
@@ -12,7 +12,8 @@
  * @author     Matt Parker <matt@lamplightdb.co.uk>
  * @copyright  Copyright (c) 2010, Lamplight Database Systems Limited, http://www.lamplightdb.co.uk
  * @license    http://www.lamplight-publishing.co.uk/license.php   BSD License
- * @version    1.1 Update to include 'attend work' and 'add referrals' datain module functionality
+ * @version    1.2 Add/edit profile functionality
+ * @history    1.1 Update to include 'attend work' and 'add referrals' datain module functionality
  */
  
 
@@ -27,7 +28,8 @@ require_once 'Lamplight/Record/Abstract.php';
  * @copyright  Copyright (c) 2010, Lamplight Database Systems Limited, http://www.lamplightdb.co.uk
  * @license    http://www.lamplight-publishing.co.uk/license.php    BSD License
  * @author     Matt Parker <matt@lamplightdb.co.uk>
- * @version    1.1 Update to include 'attend work' and 'add referrals' datain module functionality
+ * @version    1.2 No change
+ * @history    1.1 Update to include 'attend work' and 'add referrals' datain module functionality
  * @link       http://www.lamplight-publishing.co.uk/api/phpclient.php  Worked examples and documentation for using the client library   
  *
  *
