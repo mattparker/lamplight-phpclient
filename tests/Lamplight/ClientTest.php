c2fbe48ef2819eca76b324a08afba86135030410
MatthewParker <matt@lamplightdb.co.uk>
2022-01-14 10:21:22 +0000

    Simple unit test of fetch workareas

--- tests/Lamplight/ClientTest.php
+++ tests/Lamplight/ClientTest.php
@@ -3,20 +3,52 @@
 namespace Lamplight;
 
 use Mockery as m;
+use Psr\Http\Message\ResponseInterface;
 
 class ClientTest extends m\Adapter\Phpunit\MockeryTestCase {
 
-    
-    public function test_constructor () {
+    protected $sut;
+    protected $mock_guzzle_client;
 
-        $guzzle = m::mock(\GuzzleHttp\Client::class);
-        $sut = new Client($guzzle, []);
+    public function setUp (): void {
+
+        $this->mock_guzzle_client = m::mock(\GuzzleHttp\Client::class);
+        $this->sut = new Client($this->mock_guzzle_client, ['key' => 'abc123', 'lampid' => 144, 'project' => 2]);
+
     }
 
 
-    public function test_constructor_with_config_params () {
+    public function test_constructor_with_no_params_gives_exception_on_request () {
 
-        $guzzle = m::mock(\GuzzleHttp\Client::class);
-        $sut = new Client($guzzle, ['key' => 'abc123', 'lampid' => 144, 'project' => 2]);
+        $this->sut = new Client($this->mock_guzzle_client, []);
+
+        $this->expectException(\Exception::class);
+        $this->sut->request();
+    }
+
+
+    public function test_constructor_with_config_params_but_no_requested_info_gives_exception () {
+
+        $this->expectException(\Exception::class);
+        $this->sut->request();
+
+    }
+
+    public function test_request_workareas_successfully () {
+
+        $response = m::mock(ResponseInterface::class);
+
+        $this->mock_guzzle_client->shouldReceive('request')
+            ->with(
+                'GET',
+                'https://lamplight.online/api/workarea/all/format/json',
+                [
+                    'key' => 'abc123',
+                    'lampid' => 144,
+                    'project' => 2
+                ]
+            )->andReturn($response);
+
+        $this->sut->fetchWorkarea()->fetchAll()->request();
     }
 }
